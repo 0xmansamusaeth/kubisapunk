19e902d03fc9c15e5d847050c5a5ed18147e738a
0xmansamusaeth <elijahzgambo1@gmail.com>
2026-03-12 17:04:41 +0200

    new deployment address sepolia

--- app/utils/contracts/index.ts
+++ app/utils/contracts/index.ts
@@ -1,4 +1,4 @@
 import KubisaPunkABI from './KubisaPunk.abi.json';
 
-export const KUBISAPUNK_ADDRESS = '0x532701ea0b56331529206268EaA985Dfc7Af6b16';
+export const KUBISAPUNK_ADDRESS = '0x701093952c26828A173D51c8a73B1c47d42aceC8';
 export const KUBISAPUNK_ABI = KubisaPunkABI;
